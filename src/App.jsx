2bdf9b7e592a3bec8e0d93c5712f3132cc23b69c
Himanshu Jain <himanshu.jain@hashstudioz.com>
2023-12-24 19:40:32 +0530

    initail commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,23 @@
 import "./App.css";
-import TodoForm from "./components/forms/TodoForm";
-import TodoList from "./components/TodoList";
+import LoginForm from "./components/forms/Login";
 
 function App() {
   return (
     <>
-    <div  style={{
-          width: "100%",
-          height: "100vh",
+      <div
+        style={{
+          width: "30%",
+          height: "50vh",
           display: "flex",
           justifyContent: "center",
           alignItems: "center",
           flexDirection: "column",
           background: "#FAF9F6"
-        }}>
-    <h1>TODO LIST</h1>
-      <TodoForm/>
-      <TodoList/>
-    </div>
+        }}
+      >
+        <h1>Login</h1>
+        <LoginForm />
+      </div>
     </>
   );
 }
